5adbd5b908f16352592059a258dea14392e6df85
Askar Karasaev <askar.karasaev@gmail.com>
2019-09-12 22:04:26 +0900

    Playing with twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,10 +9,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -21,4 +22,21 @@ class ArticleController
     {
         return new Response('Hello World!');
     }
+
+    /**
+	 * @Route("/news/{slug}")
+	 */
+
+    public function show($slug)
+	{
+		$comments = [
+			'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ',
+			'The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.',
+			'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy.'
+		];
+		return $this->render('article/show.html.twig', [
+			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'comments' => $comments,
+			]);
+	}
 }
